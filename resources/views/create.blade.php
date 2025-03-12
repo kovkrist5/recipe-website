812ab0c7e4b00096c16fbb64f85198f93c452767
kovkrist5 <kovacskrist5@gmail.com>
2025-03-12 13:24:52 +0100

    added comments

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -18,10 +18,34 @@
         </fieldset>
         <fieldset>
             <select name="courseId" id="">
+                
+                <option value="1">appetizer</option>
+                <option value="2">soup</option>
+                <option value="3">main dish</option>
+                <option value="4">side dish</option>
+                <option value="5">salad</option>
+                <option value="6">dessert</option>
+                <option value="7">pastry</option>
                 <option value="8">other</option>
             </select>
         </fieldset>
+
+        <fieldset>
+            <label for="name">description: </label>
+            <input type="text" name="desc" id="desc">
+        </fieldset>
+
+        <fieldset>
+            <label for="name">instructions: </label><!--same with ingredients-->
+            <ol>
+                <li>
+                    <input type="text" name="desc" id="desc">
+                    </fieldset>  
+                </li>
+            </ol>
+            <button>+</button><!--make it add another <li>-->
         <button type="submit">save</button>
+        <!--checkboxes for allergens-->
     </form>
 </body>
 </html>
