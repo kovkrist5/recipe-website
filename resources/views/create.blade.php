6c3e1f1357d0be4d88a7b1093477cece5e10e33f
firCables <leleregina14@gmail.com>
2025-03-30 22:28:54 +0200

    create page beautifully centered

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,7 +4,7 @@
 @section('title', '| Create Recipe')
 @section('css', '../css/front.css')
 
-
+<div class="recipe-form-container">
     <h1>Create Recipe</h1>
     <form action="{{ route('store') }}" method="post">
         @csrf
@@ -24,53 +24,50 @@
         <input type="file" id="image" name="image" accept="image/*" style="display: none;">
         </fieldset>
 
-
         <fieldset>
             <label for="desc"><b>Description:</b> </label>
             <input type="text" name="desc" id="desc">
         </fieldset>
 
         <fieldset>
-            <fieldset>
-                <label for="instructions"><b>Instructions:</b></label>
-                <ol id="instructionsList" class="dynamic-list" data-input-name="instructions[]">
-                    <li id="instructionsList_item_1">
-                        <input type="text" name="instructions[]" id="instructionsList_input_1">
-                        <button id="addButton" type="button" class="remove-btn" data-id="instructionsList_item_1">-</button>
-                    </li>
-                </ol>
-            </fieldset>
+            <label for="instructions"><b>Instructions:</b></label>
+            <ol id="instructionsList" class="dynamic-list" data-input-name="instructions[]">
+                <li id="instructionsList_item_1">
+                    <input type="text" name="instructions[]" id="instructionsList_input_1">
+                    <button id="addButton" type="button" class="remove-btn" data-id="instructionsList_item_1">-</button>
+                </li>
+            </ol>
+        </fieldset>
 
-            <fieldset>
-                <label for="ingredients"><b>Ingredients:</b></label>
-                <ol id="instructionsList" class="dynamic-list" data-input-name="ingredients[]">
-                    <li id="ingredientsList_item_1">
-                        <input type="text" name="ingredients[]" id="ingredientsList_input_1">
-                        <button id="addButton" type="button" class="remove-btn" data-id="ingredientsList_item_1">-</button>
-                    </li>
-                </ol>
-            </fieldset>
-            <!-- Allergens Checkboxes -->
+        <fieldset>
+            <label for="ingredients"><b>Ingredients:</b></label>
+            <ol id="instructionsList" class="dynamic-list" data-input-name="ingredients[]">
+                <li id="ingredientsList_item_1">
+                    <input type="text" name="ingredients[]" id="ingredientsList_input_1">
+                    <button id="addButton" type="button" class="remove-btn" data-id="ingredientsList_item_1">-</button>
+                </li>
+            </ol>
+        </fieldset>
 
-            <fieldset>
-
-                <div class="allergen-container">
-                    <label for="allegens"><b>Allergens:</b></label>
-                    <!--find out why it sends as null-->
+        <fieldset>
+            <div class="allergen-container">
+                <label for="allergens"><b>Allergens:</b></label>
+                <div class="allergen-grid">
                     @foreach($alg as $a)
                         <div class="allergen-item">
                             <input type="checkbox" name="allergens[]" value="{{ $a->id }}" id="{{ $a->id }}">
                             <label for="{{ $a->id }}">{{ $a->allergenName }}</label>
                         </div>
                     @endforeach
-                    <div class="allergen-container">
-            </fieldset>
-            <br>
-            <button type="submit">Save</button>
-    </form>
-    <br>
-    <!-- Include the external JavaScript file -->
-    <script src="{{ asset('js/DynamicList.js') }}"></script>
-    <!--make it more universal so we can use the same code for isnt and ing-->
+                </div>
+            </div>
+        </fieldset>
 
+        <br>
+        <button type="submit">Save</button>
+    </form>
+</div>
+
+<br>
+<script src="{{ asset('js/DynamicList.js') }}"></script>
 @endsection
